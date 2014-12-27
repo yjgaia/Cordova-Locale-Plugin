b4bf9e143715c775bb40b52b28ca5dab7fb558b1
Hanul <hanul@hanul.me>
2014-12-27 22:51:49 +0900

    callback

--- www/Locale.js
+++ www/Locale.js
@@ -1,27 +1,27 @@
 module.exports = {
 
-	getLanguage : function() {
-		cordova.exec(undefined, undefined, 'CordovaLocalePlugin', 'getLanguage', []);
+	getLanguage : function(callback) {
+		cordova.exec(callback, undefined, 'CordovaLocalePlugin', 'getLanguage', []);
 	},
-	getISO3Language : function() {
-		cordova.exec(undefined, undefined, 'CordovaLocalePlugin', 'getISO3Language', []);
+	getISO3Language : function(callback) {
+		cordova.exec(callback, undefined, 'CordovaLocalePlugin', 'getISO3Language', []);
 	},
-	getCountry : function() {
-		cordova.exec(undefined, undefined, 'CordovaLocalePlugin', 'getCountry', []);
+	getCountry : function(callback) {
+		cordova.exec(callback, undefined, 'CordovaLocalePlugin', 'getCountry', []);
 	},
-	getISO3Country : function() {
-		cordova.exec(undefined, undefined, 'CordovaLocalePlugin', 'getISO3Country', []);
+	getISO3Country : function(callback) {
+		cordova.exec(callback, undefined, 'CordovaLocalePlugin', 'getISO3Country', []);
 	},
-	toString : function() {
-		cordova.exec(undefined, undefined, 'CordovaLocalePlugin', 'toString', []);
+	toString : function(callback) {
+		cordova.exec(callback, undefined, 'CordovaLocalePlugin', 'toString', []);
 	},
-	getDisplayLanguage : function() {
-		cordova.exec(undefined, undefined, 'CordovaLocalePlugin', 'getDisplayLanguage', []);
+	getDisplayLanguage : function(callback) {
+		cordova.exec(callback, undefined, 'CordovaLocalePlugin', 'getDisplayLanguage', []);
 	},
-	getDisplayCountry : function() {
-		cordova.exec(undefined, undefined, 'CordovaLocalePlugin', 'getDisplayCountry', []);
+	getDisplayCountry : function(callback) {
+		cordova.exec(callback, undefined, 'CordovaLocalePlugin', 'getDisplayCountry', []);
 	},
-	getDisplayName : function() {
-		cordova.exec(undefined, undefined, 'CordovaLocalePlugin', 'getDisplayName', []);
+	getDisplayName : function(callback) {
+		cordova.exec(callback, undefined, 'CordovaLocalePlugin', 'getDisplayName', []);
 	}
 };
